4b3d6c1ac23139e4bb52e6e2501564602ed4ddde
TooSchoolForCool <zeyuz@outlook.com>
2016-12-04 11:04:55 +0800

    add HOG extractor

--- imageProcessor.py
+++ imageProcessor.py
@@ -24,10 +24,27 @@ def HOG(img):
 	padding = (8,8)
 	locations = [[0,0]]
 
-	# hist = hog.compute(img, winStride, padding, locations)
-	hist = hog.compute(img)
+	hist = hog.compute(img, winStride, padding, locations)
 
 	return hist
+
+def getHOGDescriptor(winSize):
+	blockSize = (16,16)
+	blockStride = (8,8)
+	cellSize = (8,8)
+	nbins = 9
+	derivAperture = 1
+	winSigma = 4. 
+	histogramNormType = 0
+	L2HysThreshold = 2.0000000000000001e-01
+	gammaCorrection = 0
+	nlevels = 64
+	
+	hog = cv2.HOGDescriptor(winSize,blockSize,blockStride,cellSize,nbins,derivAperture,winSigma,
+	                         histogramNormType,L2HysThreshold,gammaCorrection,nlevels)
+
+	return hog.compute
+
 
 # loadImages images from the root directory and resize the image 
 # in width and height respectively.
